b67a42a10fb9582099e41d1498f84839a56d8c5b
GreerCaleb-FS <cdgreer@student.fullsail.edu>
2025-05-10 23:56:41 -0500

    Initial Commit
    Starting Repo setup.

---
+++ app/app.js
@@ -0,0 +1,17 @@
+//app.js
+const express = require("express");
+const app = express();
+const router = require("./routes");
+
+app.use(express.json());
+
+("localhost:3000/");
+app.get("/", (req, res) => {
+  console.log("GET");
+  res.json({ message: "Hello Worlds!" });
+});
+
+("localhost:3000/api");
+app.use("/api", router);
+app.use(express.json());
+module.exports = app;
